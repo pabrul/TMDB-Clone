c9a10c76fc18c290cc78fab384cccde1aea738cf
pabrul-mdt <pablo.batista@onovomercado.com.br>
2024-09-06 18:10:37 -0300

    feat: adiciona componentes moleculares

---
+++ TMDB-clone/src/components/molecules/TabSelector.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { defineProps, defineEmits } from "vue";
+
+const props = defineProps({
+  tabs: {
+    type: Array,
+    required: true,
+  },
+  activeTab: {
+    type: String,
+    required: true,
+  },
+});
+
+const emit = defineEmits(["change"]);
+
+const changeTab = (tab) => {
+  emit("change", tab);
+};
+</script>
+
+<template>
+  <div class="mb-8 flex justify-center space-x-4">
+    <button
+      v-for="tab in tabs"
+      :key="tab.value"
+      @click="changeTab(tab.value)"
+      :class="[
+        'px-4 py-2 rounded-full transition-colors duration-200',
+        activeTab === tab.value
+          ? 'bg-tmdb-light-blue text-white'
+          : 'bg-gray-200 text-gray-700 hover:bg-gray-300',
+      ]"
+    >
+      {{ tab.label }}
+    </button>
+  </div>
+</template>
--- TMDB-clone/src/components/organisms/MovieList.vue
+++ TMDB-clone/src/components/organisms/MovieList.vue
@@ -1,20 +1,55 @@
 <script setup>
-import { onMounted } from "vue";
+import { ref, onMounted, computed } from "vue";
 import { useMovieStore } from "../../stores/movieStore";
 import MovieCard from "../molecules/MovieCard.vue";
+import TabSelector from "../molecules/TabSelector.vue";
 
 const movieStore = useMovieStore();
+const activeTab = ref("trending");
+
+const tabs = [
+  { value: "trending", label: "Tendências" },
+  { value: "popular", label: "Os Mais Populares" },
+];
+
+const fetchMovies = () => {
+  if (activeTab.value === "trending") {
+    movieStore.fetchTrendingMovies();
+  } else {
+    movieStore.fetchPopularMovies();
+  }
+};
 
 onMounted(() => {
-  movieStore.fetchTrendingMovies();
+  fetchMovies();
+});
+
+const changeTab = (tab) => {
+  activeTab.value = tab;
+  fetchMovies();
+};
+
+const movies = computed(() => {
+  return activeTab.value === "trending"
+    ? movieStore.trendingMovies
+    : movieStore.popularMovies;
+});
+
+const title = computed(() => {
+  return activeTab.value === "trending"
+    ? "Filmes em Tendência"
+    : "Filmes Mais Populares";
 });
 </script>
 
 <template>
   <div>
+    <TabSelector :tabs="tabs" :activeTab="activeTab" @change="changeTab" />
+
     <h1 class="text-4xl font-bold mb-8 text-center text-white">
-      Filmes em Tendência
+      {{ title }}
     </h1>
+
     <div v-if="movieStore.loading" class="text-center py-12">
       <p class="text-3xl text-white">Carregando filmes...</p>
     </div>
@@ -25,11 +60,7 @@ onMounted(() => {
       v-else
       class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8"
     >
-      <MovieCard
-        v-for="movie in movieStore.trendingMovies"
-        :key="movie.id"
-        :movie="movie"
-      />
+      <MovieCard v-for="movie in movies" :key="movie.id" :movie="movie" />
     </div>
   </div>
 </template>
