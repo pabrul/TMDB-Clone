41fbaaab429c12c6deb60d73d13f5e29dcb5d038
pabrul-mdt <pablo.batista@onovomercado.com.br>
2024-09-06 17:33:30 -0300

    feat: adiciona layouts e páginas

---
+++ TMDB-clone/src/layouts/MainLayout.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Header from "../components/organisms/Header.vue";
+import Footer from "../components/organisms/Footer.vue";
+</script>
+
+<template>
+  <div
+    class="min-h-screen bg-gradient-to-br from-tmdb-dark-blue to-tmdb-light-blue text-white flex flex-col"
+  >
+    <Header />
+    <main class="container mx-auto px-4 py-8 flex-grow">
+      <slot></slot>
+    </main>
+    <Footer />
+  </div>
+</template>
---
+++ TMDB-clone/src/pages/HomePage.vue
@@ -0,0 +1,10 @@
+<script setup>
+import MovieList from "../components/organisms/MovieList.vue";
+</script>
+
+<template>
+  <div class="container mx-auto px-4 py-8">
+    <h1 class="text-3xl font-bold mb-6">TMDB Clone</h1>
+    <MovieList />
+  </div>
+</template>
