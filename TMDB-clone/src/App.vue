7ca6dbf4575bde27bfc6a5cab6ff11e2501fb997
pabrul-mdt <pablo.batista@onovomercado.com.br>
2024-09-06 17:32:53 -0300

    feat: adiciona arquivos de estilo e configuração inicial do Vue

---
+++ TMDB-clone/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import MainLayout from "./layouts/MainLayout.vue";
+import HomePage from "./pages/HomePage.vue";
+</script>
+
+<template>
+  <MainLayout>
+    <HomePage />
+  </MainLayout>
+</template>
+
+<style>
+body {
+  font-family: "Montserrat", sans-serif;
+}
+</style>
